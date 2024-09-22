8577a5eab94e688265726fe0b1cacbb86f3015f0
Ruchi Jha <ruchijha139@gmail.com>
2024-09-22 11:34:24 +0530

    Property Binding

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -12,7 +12,4 @@ export class DashboardItemComponent {
   @Input({required: true}) image!: {src: string, alt:string};
   @Input({required: true}) title!: string;
 
-  // image = input.required<{src: string, alt:string}>();
-  // title = input.required<string>();
-
 }
